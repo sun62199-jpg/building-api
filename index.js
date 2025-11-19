26169eeff5b91076b9a0421f3879502b47e38ff0
sun62199-jpg <sun62199@gmail.com>
2025-11-19 14:36:03 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-// 1. 기본 세팅 v2.6 251119
+// 1. 기본 세팅 v2.7 251119
+// 1. 기본 세팅
 const express = require("express");
 const path = require("path");
 require("dotenv").config();
@@ -693,4 +694,3 @@ app.get("/", (req, res) =>
 app.listen(PORT, () =>
     console.log(`서버 실행 중 ▶ http://localhost:${PORT}`)
 );
-
